9ff95d0fb53f6e82480252b8ca48fdef0b15cd90
HCHogan <ysh2291939848@outlook.com>
2023-02-01 00:20:37 +0800

    drag

---
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -0,0 +1,43 @@
+//
+//  EmojiArtDocumentView.swift
+//  EmojiArt
+//
+//  Created by Hank on 2023/1/31.
+//
+
+import SwiftUI
+
+struct EmojiArtDocumentView: View {
+    @ObservedObject var document: EmojiArtDocument
+    var body: some View {
+        VStack {
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(EmojiArtDocument.palette.map{ String($0)}, id:\.self) { emoji in
+                        Text(emoji)
+                            .font(Font.system(size: self.defaultEmojiSize))
+                    }
+                }
+            }
+            .padding(.horizontal)
+            Rectangle()
+                .foregroundColor(.yellow)
+                .edgesIgnoringSafeArea([.horizontal, .bottom])
+                .onDrop(of: ["public.image"], isTargeted: nil) { providers, location in
+                    return self.drop(providers: providers)
+                }
+        }
+        
+    }
+    
+    private func drop(providers:[NSItemProvider]) -> Bool {
+        let found = providers.loadFirstObject(ofType: URL.self) { url in
+            print("dropped \(url)")
+            self.document.setBackgroundURL(url)
+        }
+        return found
+    }
+    
+    let defaultEmojiSize: CGFloat = 40
+}
+
