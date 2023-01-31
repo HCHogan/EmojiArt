dc3e97465a8251b2a8dfb10668192760662a6fd2
HCHogan <ysh2291939848@outlook.com>
2023-01-31 15:47:05 +0800

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Hank on 2023/1/31.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
